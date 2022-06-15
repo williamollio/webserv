07262bd6c36a7968bcfa31c7023dfc921a413c6d
mhahn <mhahn@student.42heilbronn.de>
2022-06-15 19:44:42 +0200

    Added CGICall class

---
+++ src/CGICall.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Manuel Hahn on 6/15/22.
+//
+
+#include "CGICall.hpp"
+
+CGICall::CGICall(HTTPRequest & request) : CGIResponse(request) {}
+
+CGICall::~CGICall() {}
+
+void CGICall::run(Socket &socket) {
+    // TODO
+}

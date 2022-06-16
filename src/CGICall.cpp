cb22383b0479091076a82f58563a14c96751f96a
mhahn <mhahn@student.42heilbronn.de>
2022-06-16 18:08:05 +0200

    Made use of the updates

--- src/CGICall.cpp
+++ src/CGICall.cpp
@@ -14,8 +14,7 @@ CGICall::~CGICall() {}
 void CGICall::run(Socket & socket) {
     // TODO
     try {
-        std::map<std::string, std::string> vars = URI(
-                "/folder/file.php/search?q=test&test=true&test").getVars(); // = _request.getURI().getVars();
+        std::map<std::string, std::string> vars = _request->getURI().getVars();
     } catch (URISyntaxException & ex) {
         std::cerr << ex.what() << std::endl;
         throw HTTPException(400);

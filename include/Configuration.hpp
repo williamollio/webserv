a2cd7e1518582f28b39beff41f5a9ec62e9b992c
jkasper <jkasper@student.42heilbronn.de>
2022-06-17 17:13:03 +0200

    parser structure complete, readvalue functions missing

--- include/Configuration.hpp
+++ include/Configuration.hpp
@@ -10,33 +10,12 @@ private:
 	typedef std::vector<std::string>	vectorInt;
 	typedef std::map<int, std::string>	intMapString;
 
-	class Tree {
-	private:
-		Tree*	next;
-		Tree*	prev;
-		Tree*	child;
-		Tree*	parent;
-		enum	type {boolean, integer, string, vectorString, vectorInt, intMapString};
-	public:
-		Tree();
-		~Tree();
+	enum word {server, w_errortype};
+	enum server_word {name, port, location, location_error, location_log, file_acc, s_errortype};
+	size_t			e_line;
 
-	};
-
-	template<class T>
-	class	Token {
-	private:
-		typedef T*	type_pointer;
-	public:
-		Token(const std::string& name, type_pointer target, void (*read_token_fn) (const std::string&, size_t));
-		~Token() {};
-		void	(*read_t_token) (const std::string&, size_t);
-		const std::string	token_name;
-		const type_pointer	token_target;		///WARNING: token_target can be NULL!
-	};
-
-
-	vectorString	_token_list;
+	enum word	conf_token_cmp(const std::string& word);
+	enum server_word	server_token_cmp(const std::string& word);
 
 	vectorString	_server_names;					///standard localhost:PORT
 	vectorInt		_ports;							///standard 80
@@ -45,13 +24,28 @@ private:
 	std::string		_server_location_log;
 	bool			_accept_file;					///standard OFF
 
+	void	parse_server(std::fstream& file);
+	void	get_next_delim_char(std::fstream& file, char delim);
+
+
+	void	parse_vec_str(std::fstream& file, vectorString& output);		//TODO
+	void	parse_vec_int(std::fstream& file, vectorInt& output);			//TODO
+	void	parse_map_int_str(std::fstream& file, intMapString& output);	//TODO
+	void	parse_str(std::fstream& file, std::string& output);				//TODO
+	void	parse_bool(std::fstream& file, bool& output);					//TODO
 public:
 	Configuration();
 	~Configuration();
-	void	load_config_file(int fd);
-	void	load_config_file(std::string path);
-private:
-	void load_token_list();
+	void	load_config_file(std::string& path);
+
+	class UnexpectedToken : public std::exception {
+	private:
+		size_t	_line;
+	public:
+		UnexpectedToken(size_t _in_line) _NOEXCEPT;
+		~UnexpectedToken() _NOEXCEPT {};
+		const char *what() const _NOEXCEPT;
+	};
 };
 
 
--- src/config/Configuration.cpp
+++ src/config/Configuration.cpp
@@ -1,17 +1,141 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include "Configuration.hpp"
 
 
-Configuration::Configuration() {}
+Configuration::Configuration() : e_line(0) {
+	///TODO: initialize standard values
+}
 Configuration::~Configuration() {
 	//free token list
 }
 
-template<class T>
-Configuration::Token<T>::Token(const std::string &name, Configuration::Token<T>::type_pointer target, void (*read_token_fn) (const std::string&, size_t)) : token_name(name), token_target(target) {
-	read_t_token = read_token_fn;
+Configuration::word Configuration::conf_token_cmp(const std::string& word) {
+	if (word == "server")
+		return server;
+	else {
+		return w_errortype;
+	}
+}
+
+void	clearws(std::string& line) {
+	std::string::iterator	l = line.end();
+	for (std::string::iterator	it = line.begin(); it != l; it++) {
+		if (*it == ' ' || *it == '	') {
+			line.erase(it++);
+		}
+	}
+}
+
+size_t	find_delim(std::string& line) {
+	size_t	focc = 0;
+	focc = line.find(' ');
+	if (line.find('	') < focc)
+		focc = line.find('	');
+	if (line.find('{') < focc)
+		focc = line.find('{');
+	if (line.find('}') < focc)
+		focc = line.find('}');
+	if (line.find(':') < focc)
+		focc = line.find(':');
+	if (line.find('#') < focc)
+		focc = line.find('#');
+	return focc;
+}
+
+std::string	getword(std::string& line) {
+	clearws(line);
+	std::string word = line.substr(0, find_delim(line));
+	return word;
 }
 
 
-void Configuration::load_token_list() {
 
+void	Configuration::load_config_file(std::string& path) {
+	std::fstream	file(path);
+	std::string		line;
+	while (file.peek() != EOF) {
+		std::getline(file, line);
+		switch (conf_token_cmp(getword(line))) {
+			case server:
+				parse_server(file);
+				break;
+			default:
+				throw UnexpectedToken(e_line);
+		}
+		e_line++;
+	}
+}
+
+
+Configuration::server_word Configuration::server_token_cmp(const std::string &word) {
+	if (word == "server_names" || word == "server_name")
+		return name;
+	if (word == "port" || word == "ports")
+		return port;
+	if (word == "loc" || word == "locations" || word == "file_location")
+		return location;
+	if (word ==  "error_page_location" || word == "error_page_locations" || word == "error_pages")
+		return location_error;
+	if (word == "log" || word == "log_location" || word == "location_log")
+		return location_log;
+	if (word == "accept_files" || word == "upload_files")
+		return file_acc;
+	else
+		return s_errortype;
+}
+
+void	Configuration::get_next_delim_char(std::fstream& file, char delim) {
+	char c;
+	while (file.peek() != EOF) {
+		file.get(c);
+		if (c == delim)
+			return;
+		else if (c == ' ' || c == '	')
+			continue ;
+		else if (c == '\n')
+			e_line++;
+		else
+			throw UnexpectedToken(e_line);
+	}
+	throw UnexpectedToken(e_line);
+}
+
+
+
+void Configuration::parse_server(std::fstream& file) {
+	std::string		line;
+
+	get_next_delim_char(file, '{');
+	while (file.peek() != EOF) {
+		std::getline(file, line);
+		switch (server_token_cmp(getword(line))) {
+			case name:
+				parse_vec_str(file, _server_names);						break;
+			case port:
+				parse_vec_int(file, _ports);							break;
+			case location:
+				parse_vec_str(file, _server_locations);					break;
+			case location_error:
+				parse_map_int_str(file, _server_locations_error_pages);	break;
+			case location_log:
+				parse_str(file, _server_location_log);					break;
+			case file_acc:
+				parse_bool(file, _accept_file);							break;
+			default:
+				throw UnexpectedToken(e_line);
+		}
+		e_line++;
+	}
+}
+
+
+//CLASS UNEXPECTED-TOKEN
+Configuration::UnexpectedToken::UnexpectedToken(size_t _in_line) _NOEXCEPT : _line(_in_line) { }
+
+const char *Configuration::UnexpectedToken::what() const _NOEXCEPT {
+	std::stringstream ret;
+	ret << "unexpected token in line: " << _line;
+	return ret.str().c_str();
 }

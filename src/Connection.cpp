ab22cacf58590ee61ed1e0dad5613a4f33880939
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-06-21 13:38:45 +0200

    Fixed indentation

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -7,27 +7,27 @@
 
 Connection::Connection() : _timeout(INFTIM), address(), _fds()
 {
-	addrlen = sizeof(address);
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
+    addrlen = sizeof(address);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-		throw IOException("Could not create socket!");
-	on = 1;
-	setsockopt (server_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof (on));
-	fcntl(server_fd, F_SETFL, O_NONBLOCK);
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-		throw IOException("Could not bind file descriptor to the address!");
-	if (listen(server_fd, 10) < 0)
-		throw IOException("Cannot listen on the socket descriptor!");
-	_initialization_poll();
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        throw IOException("Could not create socket!");
+    on = 1;
+    setsockopt (server_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof (on));
+    fcntl(server_fd, F_SETFL, O_NONBLOCK);
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+        throw IOException("Could not bind file descriptor to the address!");
+    if (listen(server_fd, 10) < 0)
+        throw IOException("Cannot listen on the socket descriptor!");
+    _initialization_poll();
 }
 
 void Connection::_initialization_poll()
 {
-	_fds[0].fd = server_fd;
-	_fds[0].events = POLLIN;
+    _fds[0].fd = server_fd;
+    _fds[0].events = POLLIN;
 }
 
 static void forceRemoveHTTPReader(HTTPReader* & reader) {
@@ -40,20 +40,22 @@ Connection::~Connection() {
 
 void Connection::establishConnection()
 {
-	int rc;
-	int nfds = 1;
-	int current_size;
-	bool end_server = false;
+    int rc;
+    int nfds = 1;
+    int current_size;
+    bool end_server = false;
 
-	do {
-		rc = poll(_fds, nfds, _timeout);
-		if (rc <= 0) {
-			std::cout << "poll() failed or timeout" << std::endl;
-			break;
-		}
-		current_size = nfds;
-		for (int i = 0; i < current_size; i++)
-    	{
+	for (int i = 1; i < 200; ++i)
+            assert(_fds[i].fd == 0);
+    do {
+        rc = poll(_fds, nfds, _timeout);
+        if (rc <= 0) {
+            std::cout << "poll() failed or timeout" << std::endl;
+            break;
+        }
+        current_size = nfds;
+        for (int i = 0; i < current_size; i++)
+        {
       	    if (_fds[i].revents == 0)
                 continue;
             if(_fds[i].revents != POLLIN)
@@ -80,11 +82,10 @@ void Connection::establishConnection()
 	    }
         cleanReaders();
     } while (!end_server);
-    for (int i = 0; i < nfds; i++)
-  	{
+    for (int i = 0; i < nfds; i++) {
     	if(_fds[i].fd >= 0)
     	    close(_fds[i].fd);
-  	}
+    }
 }
 
 static void maybeDeleteReader(HTTPReader* & reader) {

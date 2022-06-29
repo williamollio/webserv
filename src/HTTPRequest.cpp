167a0418442ae99ad4f0d9bc50547cf64fef54ab
mhahn <mhahn@student.42heilbronn.de>
2022-06-29 16:11:44 +0200

    Fixed compile error

--- src/HTTPRequest.cpp
+++ src/HTTPRequest.cpp
@@ -120,7 +120,8 @@ HTTPRequest::HTTPRequest(HTTPRequest::TYPE type, std::vector<std::string> &file,
 
 	if (_content_length != 0 && _content_type.empty()) {
 
-		throw HTTPException(400);
+        throw HTTPException(400);
+    }
 	else if (_content_length != 0  || isChunkedRequest(raw)) {
 		_content = true;
 		set_payload(raw, _socket);

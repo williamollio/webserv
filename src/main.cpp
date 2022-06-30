89f8d11772b62b29828bc67fcbf42ae781f7a13f
mhahn <mhahn@student.42heilbronn.de>
2022-06-30 17:34:05 +0200

    Added config file to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include "Connection.hpp"
-#include "Socket.hpp"
 #include "Configuration.hpp"
 
-int main() {
+int main(const int argc, const char ** argv) {
+    if (argc != 2) {
+        std::clog << "Missing configuration file!"  << std::endl
+                  << "Usage: webserv <config file>" << std::endl;
+        return EXIT_FAILURE;
+    }
     signal(SIGPIPE, SIG_IGN);
 	try
 	{
-//		Configuration	config("server.conf");
-        Configuration::getInstance().load_config_file("server.conf");
-		// std::cout << config;
-		// std::cout << "config loaded!" << std::endl;
-		Connection	server_connection;
+        Configuration::getInstance().load_config_file(argv[1]);
+		Connection server_connection;
 		server_connection.establishConnection();
 	}
 	catch(const std::exception& e)

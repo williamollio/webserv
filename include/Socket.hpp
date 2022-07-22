27a96078d455bd8b8def952b95a14aa38999b8f2
mhahn <mhahn@student.42heilbronn.de>
2022-07-22 14:23:30 +0200

    Added buffered reading to the socket

--- include/Socket.hpp
+++ include/Socket.hpp
@@ -1,27 +1,28 @@
 #pragma once
 
 #include <string>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <unistd.h>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <fcntl.h>
 #include "IOException.hpp"
 
+#define BUFFER_SIZE 65536
+
 class Socket {
-	private:
-		int			_fd;
-		std::string _type_header;
-		std::string _filename;
-	public:
-		Socket();
-		Socket(int fd) throw (IOException);
-		~Socket();
-		std::string read_socket() throw (IOException);
-		void send_header(const std::string & type);
-		void send(const std::string & content) throw (IOException);
-		void close_socket() const throw(IOException);
-		int get_fd() const _NOEXCEPT;
+    int    _fd;
+    size_t _read_index;
+    size_t _buffer_fill;
+    char   _buffer[BUFFER_SIZE];
+
+    void read_buffer() throw (IOException);
+
+public:
+    Socket();
+    Socket(int fd) throw (IOException);
+    ~Socket();
+
+    char        read()                            throw (IOException);
+    ssize_t     write(const std::string &)        throw (IOException);
+    ssize_t     write(const char *, size_t)       throw (IOException);
+    ssize_t     write(char)                       throw (IOException);
+    void        close()                     const throw (IOException);
+    int         get_fd()                    const _NOEXCEPT;
 };
--- src/Socket.cpp
+++ src/Socket.cpp
@@ -4,13 +4,50 @@ Socket::Socket() {}
 
 Socket::~Socket() {}
 
-Socket::Socket(int fd) throw (IOException) : _fd(fd)
+Socket::Socket(int fd) throw (IOException)
+    : _fd(fd), _read_index(0), _buffer()
 {
 	if (fd < 0)
 		throw IOException("Invalid socket descriptor!");
 }
 
-std::string Socket::read_socket() throw (IOException)
+ssize_t Socket::write(char data) throw(IOException) {
+    ssize_t ret;
+    if ((ret = ::write(_fd, &data, 1)) < 0) {
+        throw IOException("Could not write the data!");
+    }
+    return ret;
+}
+
+ssize_t Socket::write(const char * buffer, size_t size) throw(IOException) {
+    ssize_t ret;
+    if ((ret = ::write(_fd, buffer, size)) < 0) {
+        throw IOException("Could not write the data!");
+    }
+    return ret;
+}
+
+ssize_t Socket::write(const std::string & data) throw(IOException) {
+    return write(data.c_str(), data.size());
+}
+
+void Socket::read_buffer() throw(IOException) {
+    _read_index = 0;
+    ssize_t tmp = ::read(_fd, _buffer, BUFFER_SIZE);
+    if (tmp < 0) {
+        throw IOException("Could not read any data!");
+    }
+    _buffer_fill = tmp;
+}
+
+char Socket::read() throw(IOException) {
+    if (_read_index == _buffer_fill) {
+        read_buffer();
+    }
+    return _buffer_fill == 0 ? 0 : _buffer[_read_index++];
+}
+
+/*std::string Socket::read() throw (IOException)
 {
 	char*	read_buffer = new char[30001];
 	u_long	amount = 0;
@@ -20,14 +57,14 @@ std::string Socket::read_socket() throw (IOException)
 	std::string tmp = std::string(read_buffer);
 	delete[] read_buffer;
 	return(tmp);
-}
+}*/
 
-void Socket::send_header(const std::string & type)
+/*void Socket::send_header(const std::string & type)
 {
 	_type_header = type;
-}
+}*/
 
-void Socket::send(const std::string & content) throw (IOException)
+/*void Socket::send(const std::string & content) throw (IOException)
 {
 	#if DEBUG
 		std::cout << "\n\n"<< std::endl;
@@ -36,11 +73,11 @@ void Socket::send(const std::string & content) throw (IOException)
         if (write(_fd, content.c_str() + i, 1) < 0)
             throw (IOException("Could not send the data! Data: " + content));
     }
-}
+}*/
 
-void Socket::close_socket() const throw (IOException)
+void Socket::close() const throw (IOException)
 {
-	if (close(_fd) < 0)
+	if (::close(_fd) < 0)
         throw IOException("Could not close socket!");
 }
 

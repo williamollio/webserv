fad33fc5c55040c139220daf33407943deef8e50
mhahn <mhahn@student.42heilbronn.de>
2022-08-16 18:17:02 +0200

    Documented Connection.hpp

--- include/Connection.hpp
+++ include/Connection.hpp
@@ -10,19 +10,19 @@
 /// The connection end point of the server. Handles all the connections.
 class Connection {
     /// First: Port --- Second: Server fd.
-    std::map<int, int> _server_fds;
+    std::map<int, int>        _server_fds;
     std::map<int, Runnable *> _fd_mapping;
     /// First: client fd --- Second: server fd
-    std::map<int, int>      _connection_pairs;
-    std::list<HTTPReader *> _readers;
+    std::map<int, int>         _connection_pairs;
+    std::list<HTTPReader *>    _readers;
     /// The timeout in milliseconds.
-    int                _timeout;
+    int                        _timeout;
     /// The number of file descriptors inside of the polling array.
-    nfds_t             _nfds;
+    nfds_t                     _nfds;
     /// The polling array.
-    struct pollfd      _fds[NUM_FDS];
-    static Connection * currentInstance;
-    unsigned long connections;
+    struct pollfd              _fds[NUM_FDS];
+    static Connection *        currentInstance;
+    unsigned long              connections;
 
     /// Compresses the polling array. Upon return, all removable contents are removed.
     void _clear_poll_array();
@@ -49,11 +49,24 @@ class Connection {
      */
     void handle(nfds_t i);
 
-    void printPollArray()                                       _NOEXCEPT;
-	void denyConnection(const int fd, HTTPReader * reader = NULL , const int errorCode = 429) _NOEXCEPT;
+    /**
+     * Prints the contents of the polling error if DEBUG is 2.
+     */
+    void printPollArray() _NOEXCEPT;
+	void denyConnection(int fd, HTTPReader * reader = NULL , int errorCode = 429) _NOEXCEPT;
 
+    /**
+     * Garbage collects the HTTPReader instances using a variant of the mark and sweep algorithm.
+     */
     void _clean_readers();
 
+    /**
+     * Removes the given file descriptor from the polling array and removes the file descriptor
+     * to Runnable mapping.
+     *
+     * @param fd The file descriptor to remove.
+     * @return Whether the file descriptor was successfully removed.
+     */
     bool remove_fd(int fd);
 
 public:
@@ -61,8 +74,25 @@ public:
     ~Connection();
 
     void establishConnection();
+
+    /**
+     * Adds a file descriptor to the polling array. The given file descriptor is mapped to the
+     * given Runnable pointer, which must not be null. If the given file descriptor is already
+     * mapped, the mapping is replaced by the given one. Note: the old Runnable is not notified
+     * about the removal of the mapping.
+     *
+     * @param fd The file descriptor that should be added to the poll array.
+     * @param reader The object that is responsible for the file descriptor.
+     * @param read Whether the file descriptor should be checked for reading or writing.
+     * @return If the file descriptor was successfully added to the polling array.
+     */
     bool add_fd(int fd, Runnable * reader, bool read = true);
 
+    /**
+     * Returns the currently active Connection instance.
+     *
+     * @return The instance that is currently handling the polling loop.
+     */
     static Connection & getInstance();
 };
 
